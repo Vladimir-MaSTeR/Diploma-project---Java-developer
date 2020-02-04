2ee15ccc8bb751f1b49cb08df19b6db292c94973
Vladimir <tsyuman87binary@gmail.com>
2020-02-05 03:31:25 +0700

    One request

--- src/main/java/controller/DefaultController.java
+++ src/main/java/controller/DefaultController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DefaultController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
